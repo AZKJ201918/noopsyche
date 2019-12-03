a1c12800a9852125e2484a8cd1790bf40e170612
pc <1522628183@qqcom>
2019-12-03 16:14:58 +0800

    1111

--- src/main/java/com/azkj/noopsyche/service/Impl/UserServiceImpl.java
+++ src/main/java/com/azkj/noopsyche/service/Impl/UserServiceImpl.java
@@ -34,8 +34,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public String login(String code, String uuid, String encryptedData, String iv) throws NoopsycheException {
-        if (code==null){
-            throw new NoopsycheException(100,"没有code");
+        if (code == null) {
+            throw new NoopsycheException(100, "没有code");
         }
         Map<String, Object> map = new HashMap<>();
         map.put("appid", Constants.appId);
@@ -156,4 +156,100 @@ public class UserServiceImpl implements UserService {
         }
         return null;
     }
+
+    public String login1(String openid, String uuid, String headimgurl, String nickName) throws NoopsycheException {
+        WxUser findUser = wxUserMapper.selectUserByOpenId(openid);
+        String userUUID = null;
+        if (findUser != null) {
+            userUUID = findUser.getUuid();
+        }
+        if (userUUID != null) {//用户信息已经初始化
+            System.out.println("openId已经存在,不需要插入");
+            WxUser user1 = new WxUser();
+            if (uuid != null) {//有分享人
+                String mySuperiorid = wxUserMapper.selectSuperioridByOpenid(openid);
+                if (mySuperiorid == null) {//没有父id
+                    if (!userUUID.equals(uuid)) {//自己不是自己的分享人
+                        user1.setUuid(userUUID);
+                        user1.setHeadimgurl(headimgurl);
+                        user1.setNickname(nickName);
+                        user1.setUuid(uuid);
+                        user1.setCreatetime(new Date());
+                        wxUserMapper.updateByPrimaryKeySelective(user1);
+                    } else {//自己是自己的分享人
+                        user1.setUuid(userUUID);
+                        user1.setHeadimgurl(headimgurl);
+                        user1.setNickname(nickName);
+                        user1.setCreatetime(new Date());
+                        wxUserMapper.updateByPrimaryKeySelective(user1);
+                    }
+                } else {//有父id
+                    user1 = new WxUser();
+                    user1.setToken(userUUID);
+                    user1.setHeadimgurl(headimgurl);
+                    user1.setNickname(nickName);
+                    user1.setCreatetime(new Date());
+                    wxUserMapper.updateByPrimaryKeySelective(user1);
+                }
+            } else {//没有分享人
+                user1 = new WxUser();
+                user1.setUuid(userUUID);
+                user1.setHeadimgurl(headimgurl);
+                user1.setNickname(nickName);
+                user1.setCreatetime(new Date());
+                wxUserMapper.updateByPrimaryKeySelective(user1);
+            }
+            Date date = new Date();
+            String format = new SimpleDateFormat("yyyy-MM-dd").format(date);
+            Date createtime = findUser.getCreatetime();
+            String format1 = "";
+            if (createtime != null) {
+                format1 = new SimpleDateFormat("yyyy-MM-dd").format(createtime);
+            }
+            if (format != null && !format.equals(format1)) {
+                visitCount.incrementAndGet();
+            }
+            redisUtil.setWxUser(findUser);
+            return userUUID;
+        } else {//用户没有初始化
+            String wxUserId = UUIDUtils.generateMost22UUID();//用户id
+            System.out.println("openId不存在,插入数据库");
+            WxUser user = new WxUser();
+            if (uuid != null) {//有分享人
+                String mySuperiorid = wxUserMapper.selectSuperioridByOpenid(openid);
+                if (mySuperiorid == null) {//没有父id,首次进入不可能自己是自己的分享人
+                    user.setToken(wxUserId);
+                    user.setHeadimgurl(headimgurl);
+                    user.setNickname(nickName);
+                    user.setOpenid(openid);
+                    user.setUuid(uuid);
+                    user.setCreatetime(new Date());
+                    wxUserMapper.insertSelective(user);
+                } else {//有父id
+                    user = new WxUser();
+                    user.setToken(wxUserId);
+                    user.setHeadimgurl(headimgurl);
+                    user.setNickname(nickName);
+                    user.setOpenid(openid);
+                    user.setCreatetime(new Date());
+                    wxUserMapper.insertSelective(user);
+                }
+            } else {//没有分享人
+                user = new WxUser();
+                user.setToken(wxUserId);
+                user.setHeadimgurl(headimgurl);
+                user.setNickname(nickName);
+                user.setOpenid(openid);
+                user.setCreatetime(new Date());
+                wxUserMapper.insertSelective(user);
+            }
+            //Integer count1 =  wxUserMapper.insertWxUser(newUser);
+            registerCount.incrementAndGet();
+            visitCount.incrementAndGet();
+            redisUtil.setWxUser(findUser);
+            return wxUserId;
+        }
+    }
+
 }
+
