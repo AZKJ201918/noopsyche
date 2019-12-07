6fc494156887c9e1a01674631c3a089c41c0d224
pc <1522628183@qqcom>
2019-12-07 17:52:51 +0800

    555

---
+++ src/main/java/com/azkj/noopsyche/entity/Commodity.java
@@ -0,0 +1,33 @@
+package com.azkj.noopsyche.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Commodity {
+    private Integer id;
+
+    private String title;
+
+    private String subtitle;
+
+    private String price;
+
+    private String detailurl;
+
+    private Integer sales;
+
+    private String url;
+
+    private Integer status;
+
+    private Integer type;
+
+    private Integer flag;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GTM+8")
+    private Date createtime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GTM+8")
+    private Date updatetime;
+}
