49e093e1116443911a16a5aa0327aae685cc91c8
wan <404602138@qq.com>
2019-12-16 11:57:25 +0800

    "宝强提交------商品规格检索"

---
+++ src/main/java/com/azkj/noopsyche/common/utils/SearchUtils.java
@@ -0,0 +1,53 @@
+package com.azkj.noopsyche.common.utils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.azkj.noopsyche.entity.Sku;
+import io.searchbox.client.http.JestHttpClient;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.search.MatchQuery;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class SearchUtils {
+
+    @Autowired
+    private JestHttpClient jestHttpClient;
+
+
+    public Sku SearchSku(String skuname) throws IOException {
+        Sku sku=new Sku();
+        SearchSourceBuilder searchSourceBuilder=new SearchSourceBuilder();
+        searchSourceBuilder.query(
+                QueryBuilders.boolQuery().must(
+                        QueryBuilders.matchQuery("skuname",skuname)
+                )
+        );
+        Search search=new Search.Builder(searchSourceBuilder.toString())
+				.addIndex("sku").addType("doc").build();
+        SearchResult result=jestHttpClient.execute(search);
+		String array= JSON.parseObject(result.getJsonString())
+				.getJSONObject("hits")
+				.getJSONArray("hits")
+				.toJSONString();
+        List<Map<String,Object>> mapList = (List<Map<String,Object>>) JSONArray.parse(array);
+        		mapList.forEach(
+				parm->{
+					 Map<String,Object> objectMap= (Map<String, Object>) parm.get("_source");
+					System.out.println(objectMap.toString());
+				}
+		);
+
+
+        return sku;
+    }
+}
---
+++ src/main/java/com/azkj/noopsyche/dao/PropertyMapper.java
@@ -0,0 +1,25 @@
+package com.azkj.noopsyche.dao;
+
+import com.azkj.noopsyche.entity.Property;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface PropertyMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Property record);
+
+    int insertSelective(Property record);
+
+    Property selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Property record);
+
+    int updateByPrimaryKey(Property record);
+
+    @Select("SELECT id,propertyname FROM property where id=#{id}")
+    List<Property> selectByPropertyAll(Integer id);
+}
