4421d79eb3b4f6f9093cd0b1c7685ce149f9a054
pc <1522628183@qqcom>
2019-12-25 15:15:40 +0800

    222625255

---
+++ src/main/java/com/azkj/noopsyche/controller/PersonController.java
@@ -0,0 +1,59 @@
+package com.azkj.noopsyche.controller;
+
+import com.azkj.noopsyche.common.constants.Constants;
+import com.azkj.noopsyche.common.exception.NoopsycheException;
+import com.azkj.noopsyche.common.resp.ApiResult;
+import com.azkj.noopsyche.entity.Person;
+import com.azkj.noopsyche.service.PersonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@CrossOrigin
+@Api(value = "个人页面模块")
+public class PersonController {
+    @Autowired
+    private PersonService personService;
+
+    @ApiOperation(value = "新增个人页面",notes = "新增个人页面",httpMethod = "POST")
+    @ApiImplicitParam
+    @PostMapping("/insertMyViem")
+    public ApiResult insertMyViem(@RequestBody Person person){
+        ApiResult result = new ApiResult<>();
+        try {
+            personService.addPerson(person);
+            result.setMessage("新增页面成功");
+        }catch (Exception e){
+            e.printStackTrace();
+            result.setMessage("后台服务器异常");
+            result.setCode(Constants.RESP_STATUS_INTERNAL_ERROR);
+        }
+        return result;
+    }
+    @ApiOperation(value = "查看个人页面",notes = "查看个人页面",httpMethod = "GET")
+    @ApiImplicitParam
+    @GetMapping("/loadMyViem")
+    public ApiResult loadMyViem(){
+        ApiResult<Person> result = new ApiResult<>();
+        try {
+            Person person=personService.findMyView();
+            result.setMessage("查看个人页面成功");
+            result.setData(person);
+        }catch (NoopsycheException e){
+            e.printStackTrace();
+            result.setMessage(e.getMessage());
+            result.setCode(e.getStatusCode());
+        }catch (Exception e){
+            e.printStackTrace();
+            log.error("SQL statement error or that place is empty" + e);
+            result.setMessage("后台服务器异常");
+            result.setCode(Constants.RESP_STATUS_INTERNAL_ERROR);
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/azkj/noopsyche/service/Impl/PersonServiceImpl.java
@@ -0,0 +1,27 @@
+package com.azkj.noopsyche.service.Impl;
+
+import com.azkj.noopsyche.common.exception.NoopsycheException;
+import com.azkj.noopsyche.dao.PersonMapper;
+import com.azkj.noopsyche.entity.Person;
+import com.azkj.noopsyche.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PersonServiceImpl implements PersonService {
+
+    @Autowired
+    private PersonMapper personMapper;
+
+    @Override
+    public void addPerson(Person person) {
+        personMapper.insertPerson(person);
+    }
+
+    @Override
+    public Person findMyView() throws NoopsycheException {
+        Person person = personMapper.selectMyView();
+        if (person==null){
+            throw new NoopsycheException("没有页面信息");
+        }
+        return person;
+    }
+}
